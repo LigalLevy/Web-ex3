4d161e0785fa00c360035995af4299f84fc2eec7
LigalLevy <37476027+LigalLevy@users.noreply.github.com>
2018-05-20 22:59:20 +0300

    layout3 responsive and static design

---
+++ includes/scripts.js
@@ -0,0 +1,11 @@
+(function() {
+    //do something like on click ()
+    /*document.getElementById("show").onclick = function(){
+7. var obj = document.getElementById("hidden");
+8. if(obj.style.display == "block")
+9.      obj.style.display = "none";
+10. else
+11.     obj.style.display = "block";
+12. };*/
+
+})();
