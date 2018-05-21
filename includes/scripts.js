41cbea7643a904915c2bf9d8320ce864f41c5280
LigalLevy <37476027+LigalLevy@users.noreply.github.com>
2018-05-22 00:24:46 +0300

    adding squares dynamic with JavaScript

--- includes/scripts.js
+++ includes/scripts.js
@@ -1,11 +1,47 @@
-(function() {
-    //do something like on click ()
-    /*document.getElementById("show").onclick = function(){
-7. var obj = document.getElementById("hidden");
-8. if(obj.style.display == "block")
-9.      obj.style.display = "none";
-10. else
-11.     obj.style.display = "block";
-12. };*/
+var plusObj;
 
-})();
+function Init() {
+    plusObj = document.getElementById('plus');
+    plusObj.addEventListener('click', onPlusClick);
+}
+
+
+var onPlusClick = function() {
+    new BoxManager();
+};
+
+function getRandomOpacity() {
+    var num = Math.random();
+    var valOfOpacity = num.toFixed(1);
+
+    return valOfOpacity;
+}
+
+function Box() {
+    var boxObj = document.createElement('article');
+    boxObj.className = "box";
+    boxObj.style.opacity = getRandomOpacity();
+    boxObj.onmouseover = function(){
+        boxObj.style.opacity = "1";
+    };
+    return boxObj;
+}
+
+function BoxManager() {
+    var iBoxNum = 4;
+    var sectionObj = null;
+
+    for(var i=0; i<iBoxNum; i++) {
+        if(sectionObj === null) {
+            sectionObj = document.createElement('section');
+            sectionObj.className = "boxSection";
+        }
+
+        var boxObj = new Box();
+        sectionObj.appendChild(boxObj);
+        console.log("box "+i+" was created");
+    }
+
+    var mainObj = document.getElementsByTagName('main');    // show parent
+    mainObj[0].appendChild(sectionObj);                     // put the section with the 4 boxes inside the main object
+}
